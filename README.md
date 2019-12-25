040cdedb138c388baddc5b555b66d920e93ff1a9
Casey Bowman <case.bowman@gmail.com>
2019-12-25 00:54:44 -0800

    Initial pacificbay

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pacificbay.io
+#pacificbay.io
 Source code for pacificbay.io
+
+---
+
+The installation instruction details here are for Ubuntu.
+
+- Install Rust 
+
+      curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+      . ~/.cargo/env
+
+- Install node, npm, and yarn
+
+      sudo snap install node --classic --channel=12
+
+- Run
+
+      yarn install
+      yarn build   
+      yarn lint
+      yarn dev
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+
+    <a href="http://umunhum.io:8877">
+      <img src="./assets/umunhum.jpg" height="169" width="300"/><br/>
+      Mount Umunuhum Project
+    </a>
+    <br/>
+    <br/>
+
+    <a href="http://losgatos.io:8888">
+      <img src="./assets/losgatos.jpeg" height="169" width="300"/><br/>
+      Los Gatos Reading Group
+    </a>
+    <div><Welcome/></div>
+
+  </div>
+
+</template>
+
+<script>
+import Welcome from './components/Welcome'
+
+export default {
+  name: 'App',
+  components: {
+    Welcome
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
